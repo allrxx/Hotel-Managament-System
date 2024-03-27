60a29aae9e3bf98bf361fb7d5020ca73572ee222
George Shaiju <65544308+GeorgeShaijuu@users.noreply.github.com>
2024-03-27 13:55:33 +0530

    .

--- Hotel Managament System/Form2.vb
+++ Hotel Managament System/Form2.vb
@@ -12,4 +12,9 @@
         Guna2GroupBox3.Visible = True
 
     End Sub
+
+    Private Sub Guna2Button4_Click(sender As Object, e As EventArgs) Handles Guna2Button4.Click
+        Form3.Show()
+        Me.Hide()
+    End Sub
 End Class
